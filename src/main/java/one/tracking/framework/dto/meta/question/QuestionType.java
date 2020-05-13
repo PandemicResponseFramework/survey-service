6dad6f23f8680f06045e2f2c7a735a40fbbb7d5c
Marko Voss <markovoss@arcor.de>
2020-05-13 15:31:49 +0200

    Added convenience method to map question type.

--- src/main/java/one/tracking/framework/dto/meta/question/QuestionType.java
+++ src/main/java/one/tracking/framework/dto/meta/question/QuestionType.java
@@ -14,4 +14,20 @@ public enum QuestionType {
   RANGE,
   TEXT,
   CHECKLIST;
+
+  public static QuestionType valueOf(final Class<? extends QuestionDto> clazz) {
+
+    if (BooleanQuestionDto.class.isAssignableFrom(clazz))
+      return BOOL;
+    if (ChecklistQuestionDto.class.isAssignableFrom(clazz))
+      return CHECKLIST;
+    if (ChoiceQuestionDto.class.isAssignableFrom(clazz))
+      return CHOICE;
+    if (RangeQuestionDto.class.isAssignableFrom(clazz))
+      return RANGE;
+    if (TextQuestionDto.class.isAssignableFrom(clazz))
+      return TEXT;
+
+    return null;
+  }
 }
